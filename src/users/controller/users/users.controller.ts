f68124fadc941243ee92bcaf5ddb840580a84046
Commit <viethd@vnext.vn>
2025-09-24 14:17:30 +0700

    adding role guards for user-related routes

--- src/users/controller/users/users.controller.ts
+++ src/users/controller/users/users.controller.ts
@@ -1,17 +1,19 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UserDTO } from 'src/dto/user.dto';
 import { UserUpdateDTO } from 'src/dto/login.dto';
 import { UsersService } from 'src/users/services/users/users.service';
+import { UserGuard } from 'src/users/guard/user/user.guard';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly userServices: UsersService) {}
     
     @Get()
+    @UseGuards(UserGuard)
     async getAllUsers() {
         return await this.userServices.getAllUsers()
     }
-
+    @UseGuards(UserGuard)
     @UsePipes(new ValidationPipe({
             forbidNonWhitelisted: true,
             transform: true
@@ -20,14 +22,17 @@ export class UsersController {
     async createUser(@Body() userData: UserDTO) {
         return await this.userServices.createUser(userData)
     }
+    @UseGuards(UserGuard)
     @Get(":id")
     async getUserByID(@Param("id") id: string) {
         return await this.userServices.getUserByID(id)
     }
+    @UseGuards(UserGuard)
     @Patch(":id")
     async updateUser(@Param("id") id:string, @Body() patchData: UserUpdateDTO) {
         return await this.userServices.updateUser(id, patchData)
     }
+    @UseGuards(UserGuard)
     @Delete(":id")
     async deleteUser(@Param("id") id:string) {
         return await this.userServices.deleteUser(id)
--- src/users/guard/user/user.guard.ts
+++ src/users/guard/user/user.guard.ts
@@ -1,11 +1,39 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Observable } from 'rxjs';
-
+import * as jwt from 'jsonwebtoken'
 @Injectable()
 export class UserGuard implements CanActivate {
+  constructor(
+      private readonly configService: ConfigService
+    ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+   const req = context.switchToHttp().getRequest()
+       let token: string | undefined;
+       if (req.headers['cookie']) {
+         const cookies = req.headers['cookie'].split(';').map(cookie => cookie.trim())
+         for (const cookie of cookies) {
+           if (cookie.startsWith('jwt=')) {
+             token = cookie.split('=')[1].trim()
+             break;
+           }
+         }
+       }
+       if (!token) {
+         throw new UnauthorizedException('No token found')
+       }
+       try {
+         const decoded = jwt.verify(token, this.configService.get<string>('JWT_SECRET') || 'secretdnd')
+         req.user = decoded
+         if (!req.user || req.user.role !== 'admin') {
+          throw new ForbiddenException('Admins only');
+         }
+         return true
+       } catch (err) {
+         console.error('JWT error:', err)
+         throw new UnauthorizedException('Invalid token')
+       }
   }
 }
